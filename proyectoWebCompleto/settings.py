dc621254e286710c2417082281ab4729b8ca6ce0
Ricardo-we <totigamer75@gmail.com>
2021-08-26 11:06:21 -0600

    static settings

--- proyectoWebCompleto/settings.py
+++ proyectoWebCompleto/settings.py
@@ -134,8 +134,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, "static")
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/Media/'
