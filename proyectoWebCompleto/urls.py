890f7fa7c53a551cf789f9bb3aa2e8dad9e74f6c
Ricardo-we <totigamer75@gmail.com>
2021-08-26 13:35:23 -0600

    commit

--- proyectoWebCompleto/urls.py
+++ proyectoWebCompleto/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include, re_path
 
-from django.conf import settings
+from proyectoWebCompleto import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 
