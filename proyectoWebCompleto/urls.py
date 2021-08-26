92414530e2cb10c13cbb81c33555963edeae710c
Ricardo-we <totigamer75@gmail.com>
2021-08-26 11:16:37 -0600

    Try media 5

--- proyectoWebCompleto/urls.py
+++ proyectoWebCompleto/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -27,4 +28,5 @@ urlpatterns = [
     path("tienda/", include('tienda.urls')),
     path("blog/", include('blog.urls')),
     path("contacto/", include('contacto.urls'))
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
